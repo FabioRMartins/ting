ee8e6b10546ff01d02904bbc37b818a6d0175ce9
FabioMartins <fabbio1000@gmail.com>
2023-02-04 16:48:01 -0300

    Feat: Implementando uma função exists_word e search_by_word, dentro do módulo word_search.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    exist = []
+    for i in range(len(instance)):
+        file = instance.search(i)
+        for line in file["linhas_do_arquivo"]:
+            if word in line:
+                exist.append(file["nome_do_arquivo"])
+        if len(exist) > 0:
+            return exist
+    return exist
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    exist = exists_word(word, instance)
+    if len(exist) > 0:
+        print(f"Palavra {word} encontrada nos arquivos: {exist}")
+    else:
+        print(f"Palavra {word} não encontrada")
+    return exist
