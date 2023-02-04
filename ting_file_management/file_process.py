bbc322a475a96aff6563cbf79225c97c836df32a
FabioMartins <fabbio1000@gmail.com>
2023-02-04 16:21:56 -0300

    Feat: Implementando uma função file_metadata dentro do módulo file_process capaz de apresentar as informações superficiais de um arquivo processado.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
@@ -31,4 +32,10 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position >= len(instance):
+        print("Posição inválida", file=sys.stderr)
+        return
+
+    file = instance.search(position)
+
+    print(file)
